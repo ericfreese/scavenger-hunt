856c3c339d67d616dbb56b81ce795355bed9f0ec
Eric Freese <ericdfreese@gmail.com>
2013-02-22 13:44:45 -0700

    Try to get invitation emails sending.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,10 @@ ScavengerHunt::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # For devise
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'jointhehunt.herokuapp.com' }
 
   # Enable threaded mode
   # config.threadsafe!
