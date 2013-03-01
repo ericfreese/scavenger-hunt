a2e92227210d7f1cfbccab4ba5d525f4a3255d87
Eric Freese <ericdfreese@gmail.com>
2013-03-01 15:14:38 -0700

    Remove a bunch of old stuff and add a file I forgot last commit.

--- app/models/player.rb
+++
@@ -1,4 +0,0 @@
-class Player < ActiveRecord::Base
-  belongs_to :team
-  attr_accessible :name
-end
--- db/migrate/20130216021409_create_hunts.rb
+++
@@ -1,9 +0,0 @@
-class CreateHunts < ActiveRecord::Migration
-  def change
-    create_table :hunts do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
